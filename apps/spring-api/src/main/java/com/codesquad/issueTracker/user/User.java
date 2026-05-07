595e15d8a40b39c5e802052dfd6471c13b73a846
Jon <66867608+jwpark97114@users.noreply.github.com>
2026-05-07 18:28:24 +0900

    Revert "Chore/3 project setup" (#9)
    * Revert "feat: Spring API 공통 응답 구조 및 도메인 초기 세팅"
    
    This reverts commit bc665addd8e093bfa3ab3be535b14e111e69f50f.
    
    * Revert "chore: 디자인 핸드오프 자료 추가 및 환경 설정 보강"
    
    This reverts commit 7b2115b39f5ee8e524a1bbc659f49c5f1deb5359.
    
    * Revert "chore: 워크스페이스 구성 및 React 프론트엔드 초기화"
    
    This reverts commit ce8a9258a5c9bc1ca8cdb0491b649110139f0f28.
    
    * Revert "chore: gitignore 및 Spring 프로젝트 초기 설정"
    
    This reverts commit ed4d9fd6210201a6b078ffbf5eaf8c575624a008.

--- apps/spring-api/src/main/java/com/codesquad/issueTracker/user/User.java
+++
@@ -1,7 +0,0 @@
-package com.codesquad.issueTracker.user;
-
-import org.springframework.data.relational.core.mapping.Table;
-
-//@Table(name = "users")
-public class User {
-}
--- packages/api-spec/src/mutator/axios.ts
+++
@@ -1,9 +0,0 @@
-import axios, { AxiosRequestConfig } from 'axios';
-
-export const AXIOS_INSTANCE = axios.create({
-  baseURL: 'http://localhost:8080',
-});
-
-export const customInstance = <T>(config: AxiosRequestConfig): Promise<T> => {
-  return AXIOS_INSTANCE({ ...config }).then(({ data }) => data);
-};
