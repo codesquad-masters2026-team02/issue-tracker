9004ec6dc3fbcb37fdeb4884d19d852a4228f97a
jwpark97114 <jwpark97114@gmail.com>
2026-05-12 18:20:57 +0900

    feat: 코멘트 정렬 추가

--- apps/spring-api/src/main/java/com/codesquad/issueTracker/comment/CommentRepository.java
+++ apps/spring-api/src/main/java/com/codesquad/issueTracker/comment/CommentRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 @Repository
 public interface CommentRepository extends ListCrudRepository<Comment,Long> {
 
-    List<Comment> findAllByIssueNumber(Long issueNumber);
+    List<Comment> findAllByIssueNumberOrderByCreatedAtAsc(Long issueNumber);
 }
--- apps/spring-api/src/main/java/com/codesquad/issueTracker/comment/CommentService.java
+++ apps/spring-api/src/main/java/com/codesquad/issueTracker/comment/CommentService.java
@@ -35,7 +35,7 @@ public class CommentService {
             throw new BusinessException(ErrorCode.ISSUE_NOT_FOUND);
         }
         else{
-            List<Comment> comments = commentRepo.findAllByIssueNumber(issueNumber);
+            List<Comment> comments = commentRepo.findAllByIssueNumberOrderByCreatedAtAsc(issueNumber);
             List<CommentResponse> commentResponses = comments.stream().map(CommentResponse::new).toList();
             return new CommentListResponse(issueNumber, commentResponses);
         }
