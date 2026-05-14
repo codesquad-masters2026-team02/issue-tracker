d454b5ac8d098cf8b7dc13b7142f1f580357196d
jwpark97114 <jwpark97114@gmail.com>
2026-05-14 11:38:20 +0900

    feat: Milestone, DTO 클래스 추가

--- apps/spring-api/src/main/java/com/codesquad/issueTracker/milestone/Milestone.java
+++ apps/spring-api/src/main/java/com/codesquad/issueTracker/milestone/Milestone.java
@@ -1,7 +1,25 @@
 package com.codesquad.issueTracker.milestone;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
-//@Table(name = "milestones")
+import java.time.LocalDateTime;
+
+@Table(name = "MILESTONES")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Milestone {
+    @Id
+    private Long id;
+    private String name;
+    private LocalDateTime dueDate;
+    private String description;
+    private Integer openIssueCount;
+    private Integer closedIssueCount;
+    private String status;
+    private Boolean isDeleted;
 }
