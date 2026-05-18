e72c77e8e63d3d9767f5a54beae7e9947497b2f7
jwpark97114 <jwpark97114@gmail.com>
2026-05-18 10:38:51 +0900

    feat: User 클래스 추가
    oauthProvider, oauthId, profileImageUrl 추가

--- apps/spring-api/src/main/java/com/codesquad/issueTracker/user/User.java
+++ apps/spring-api/src/main/java/com/codesquad/issueTracker/user/User.java
@@ -1,7 +1,19 @@
 package com.codesquad.issueTracker.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
-//@Table(name = "users")
+@Table(name = "users")
+@Getter
+@AllArgsConstructor
 public class User {
+    @Id
+    private Long id;
+    private String username;
+    private String password;
+    private String oauthProvider;
+    private String oauthId;
+    private String profileImageUlr;
 }
