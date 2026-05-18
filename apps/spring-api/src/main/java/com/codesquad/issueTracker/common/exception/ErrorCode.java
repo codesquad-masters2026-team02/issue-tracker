42a9acaaeb512a1bfc201242f5fd4ec85b04b5f9
jwpark97114 <jwpark97114@gmail.com>
2026-05-18 18:07:06 +0900

    feat: JWT 기반 로그인 구현
    유저 생성, 로그인 엔드포인트 구현
    토큰 정보 기반 로그인 확인 인터셉터 구현
    JWT 발급 및 확인 헬퍼 클래스 구현
    User 클래스 refresh_token 항목 추가
    USERNAME_TAKEN, USER_INFO_NOT_MATCHED, TOKEN_INVALID, LOGIN_REQUIRED 에러 추가

--- apps/spring-api/src/main/java/com/codesquad/issueTracker/common/exception/ErrorCode.java
+++ apps/spring-api/src/main/java/com/codesquad/issueTracker/common/exception/ErrorCode.java
@@ -13,7 +13,10 @@ public enum ErrorCode {
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 코멘트를 찾을 수 없습니다"),
     LABEL_NOT_FOUND(HttpStatus.NOT_FOUND, "요청하신 레이블을 찾을 수 없습니다"),
     MILESTONE_NOT_FOUND(HttpStatus.NOT_FOUND, "요청하신 마일스톤을 찾을 수 없습니다"),
-    USERNAME_TAKEN(HttpStatus.CONFLICT, "요청하신 유저네임은 이미 사용중입니다");
+    USERNAME_TAKEN(HttpStatus.CONFLICT, "요청하신 유저네임은 이미 사용중입니다"),
+    USER_INFO_NOT_MATCHED(HttpStatus.UNAUTHORIZED, "로그인 정보가 올바르지 않습니다"),
+    TOKEN_INVALID(HttpStatus.UNAUTHORIZED, "유저 정보가 일치하지 않습니다"),
+    LOGIN_REQUIRED(HttpStatus.UNAUTHORIZED, "로그인이 필요합니다");
     private final HttpStatus status;
     private final String message;
 }
